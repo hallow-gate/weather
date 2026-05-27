22c85ba628f060001e4674d433d4a6ec69df71de
hallow-gate <iraeldonationandfoundation@gmail.com>
2026-05-27 12:36:04 +0800

    Enhance service worker caching strategy
    Added skipWaiting to install event and updated cache deletion logic.

--- src/service-worker.js
+++ src/service-worker.js
@@ -2,10 +2,6 @@
 
 const CACHE_NAME = 'weather-app-v2';
 
-self.addEventListener('install', (event) => {
-  // ... rest of your code
-// service-worker.js
-
 const urlsToCache = [
   '/',
   '/index.html',
@@ -15,6 +11,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(urlsToCache))
@@ -44,16 +41,16 @@ self.addEventListener('fetch', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
+          if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
+          return null;
         })
       );
-    })
+    }).then(() => clients.claim())
   );
 });
