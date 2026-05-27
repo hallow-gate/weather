298b71102b571ce9b841df3c821cba414182d0b3
hallow-gate <iraeldonationandfoundation@gmail.com>
2026-05-27 14:19:03 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,9 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'icons/*.png'],
-      manifest: false, // Use external manifest.json
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/api\.open-meteo\.com\/.*/i,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'weather-api-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 // 1 hour
-              }
-            }
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     sourcemap: false,
