e57e98fed4fffc6851c16c1e5c89b696f5a8c69b
hallow-gate <iraeldonationandfoundation@gmail.com>
2026-05-27 12:41:54 +0800

    Update serviceWorkerRegistration.js

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,3 +1,5 @@
+
+
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
   window.location.hostname === '[::1]' ||
@@ -12,7 +14,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
 
       if (isLocalhost) {
         checkValidServiceWorker(swUrl, config);
